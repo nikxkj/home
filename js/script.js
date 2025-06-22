7d06be1e0a30ab9d4c91367926f4ddd25925826b
nik (kemdi) <49282862+nikxkj@users.noreply.github.com>
2025-06-22 10:40:45 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -14,3 +14,31 @@ burger.onclick = function () {
     htmlsec.classList.add("navopen");
   }
 };
+
+var mouseX = 520,
+  mouseY = 400,
+  eye = document.getElementById("eyes"),
+  xp = 0,
+  yp = 0;
+
+document.onmousemove = function (e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+};
+
+function scaleValue(value, from, to) {
+  var scale = (to[1] - to[0]) / (from[1] - from[0]);
+  var capped = Math.min(from[1], Math.max(from[0], value)) - from[0];
+  return ~~(capped * scale + to[0]);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  var n = scaleValue(mouseX, [0, screen.width], [-58, -42]);
+  var p = scaleValue(mouseY, [0, 1200], [25, 45]);
+  //eye.style.left = n;
+  //eye.style.top = p;
+  eye.style.transform = "translate(" + n + "%," + p + "px)";
+}
+
+animate();
