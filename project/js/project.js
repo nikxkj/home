1f425b65ad6a04312b4d1892aad78322f77cde06
nik (kemdi) <49282862+nikxkj@users.noreply.github.com>
2025-10-23 08:38:59 -0400

    clean up

--- project/js/project.js
+++
@@ -1,6 +0,0 @@
-var clrs = document.querySelectorAll(".colours span");
-for (var i = 0; i < clrs.length; i++) {
-  var bkg = clrs[i];
-  var hex = bkg.innerHTML;
-  bkg.style.backgroundColor = hex;
-}
